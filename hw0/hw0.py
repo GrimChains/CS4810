62d8fe0c821299d55e5b23cc6522580ffbed8f00
Nick <ngg3vm@virginia.edu>
2014-01-23 11:24:37 -0500

    shiz works now

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -14,15 +14,15 @@ putpixel = img.im.putpixel
 line = fread.readline()
 while (line != "") :
 	parse = line.split()
-	print parse
 	if parse[0]=="xy" :
 		putpixel((int(parse[1]), int(parse[2])), (255, 255, 255, 255))
 	elif parse[0]=="xyrgb" :
 		putpixel((int(parse[1]), int(parse[2])), (int(parse[3]), int(parse[4]), int(parse[5]), 255))
 	elif parse[0]=="xyc" :
-		print "derp"
 		hexNum = parse[3]
-		putpixel((int([parse[1]), int(parse[2])), (int(hexNum[1] + hexNum[2], 16), int(hexNum[3] + hexNum[4], 16), int(hexNum[5] + hexNum[6], 16) 255))
-		#work on this
+		red = hexNum[1] + hexNum[2]
+		blue = hexNum[5] + hexNum[6]
+		green = hexNum[3] + hexNum[4]
+		putpixel((int(parse[1]), int(parse[2])), (int(red, 16), int(green, 16), int(blue, 16), 255))
 	line = fread.readline()
 img.save(fileName)
