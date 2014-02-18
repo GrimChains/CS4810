acd604c284579d9a775fff82af2966c0270989ed
GrimChains <grimchains@gmail.com>
2014-02-18 00:00:16 -0500

    fixed trig, which also fixed fann and stripn.

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -17,7 +17,6 @@ def drawlineh( vertex1, vertex2, hexNum1, hexNum2):
 	blue2 = int(hexNum2[5] + hexNum2[6], 16)
 	drawline( vertex1, vertex2, red1, green2, blue1, red2, green2, blue2)
 
-
 def drawline( vertex1, vertex2, red1, green1, blue1, red2, green2, blue2):
 	pass
 	# Set up location vectors
@@ -63,7 +62,6 @@ def drawline( vertex1, vertex2, red1, green1, blue1, red2, green2, blue2):
 		giter += dg
 		biter += db
 		flag = not((x1 > x2 and x2 > xiter) or (x1 < x2 and x2 < xiter) or (y1 > y2 and y2 > yiter) or (y1 < y2 and y2 < yiter)) # Basically checking to see if we've over-stepped the endpoints of the line.
-
 
 def linec( parse ) :
 	if int(parse[1]) < 0 :
@@ -137,6 +135,39 @@ def drawbezier ( points, red, green, blue, incr ) :
 			vertex.append(points[i][5]) # appending alpha
 			points2.append(vertex)
 		drawbezier( points2, red, green, blue, incr )
+
+def cubicg( parse ) :
+	points = []
+	for i in range(2, len(parse)) :
+		print i
+		if int(parse[i]) < 0 :
+			points.append(vertexList[len(vertexList) + int(parse[i])])
+		else :
+			points.append(vertexList[int(parse[i]) - 1])
+
+	print "==============="
+	sharp = 0.001
+	x = 0.0
+	while x < 1 :
+		x += sharp
+		drawbezierg( points, x )
+
+def drawbezierg( points, incr ) :
+	if len(points) < 2 :
+		putpixel((math.ceil(points[0][0]), math.ceil(points[0][1])), (points[0][2], points[0][3], points[0][4], 255))
+		img.save(fileName)
+	else :
+		points2 = []
+		for i in range( 0, len(points) - 1) :
+			vertex = []
+			vertex.append(points[i][0] - (incr * (points[i][0] - points[i+1][0]))) # appending x
+			vertex.append(points[i][1] - (incr * (points[i][1] - points[i+1][1]))) # appending y
+			vertex.append(points[i][2] - (incr * (points[i][2] - points[i+1][2]))) # appending red
+			vertex.append(points[i][3] - (incr * (points[i][3] - points[i+1][3]))) # appending green
+			vertex.append(points[i][4] - (incr * (points[i][4] - points[i+1][4]))) # appending blue
+			vertex.append(points[i][5] - (incr * (points[i][5] - points[i+1][5]))) # appending alpha
+			points2.append(vertex)
+		drawbezierg( points2, incr )
 
 def tric( parse ) :
 	# Get our vertecies
@@ -213,73 +244,57 @@ def trig( parse ) :
 	points = [list1, list2, list3]
 	drawtri( points )
 
-
-
 def drawtri( points ): # This is gong to be a little different from linec, since we're going to interpolate colors in the draw funciton instead of the frontend
 	edges = []
-	r1 = points[0][2]
-	g1 = points[0][3]
-	b1 = points[0][4]
-	r2 = points[1][2]
-	g2 = points[1][3]
-	b2 = points[1][4]
-	r3 = points[2][2]
-	g3 = points[2][3]
-	b3 = points[2][4]
 	for a in range(0, 3) :
-		for b in range(1, 3) :
+		for b in range(a+1, 3) :
 			if a == b :
 				continue
-			x1 = points[a][0]
-			y1 = points[a][1]
-			r1 = points[a][2]
-			g1 = points[a][3]
-			b1 = points[a][4]
-			x2 = points[b][0]
-			y2 = points[b][1]
-			r2 = points[b][2]
-			g2 = points[b][3]
-			b2 = points[b][4]
-			length = math.sqrt(pow((x1-x2), 2) + pow((y1-y2), 2))
-			dx = abs(x1 - x2)/length
-			dy = abs(y1 - y2)/length
-			dr = abs(r1 - r2)/length
-			dg = abs(g1 - g2)/length
-			db = abs(b1 - b2)/length
-			if x1 > x2 :
-				dx *= -1
-			if y1 > y2 :
-				dy *= -1
-			if r1 > r2 :
-				dr *= -1
-			if g1 > g2 :
-				dg *= -1
-			if b1 > b2 :
-				db *= -1
-			xiter = x1
-			yiter = y1
-			riter = r1
-			giter = g1
-			biter = b1
-			flag = True
-			while flag :
-				edges.append([math.ceil(xiter), math.ceil(yiter), riter, giter, biter, 255])
-				xiter += dx
-				yiter += dy
-				riter += dr
-				giter += dg
-				biter += db
-				flag = not((x1 > x2 and x2 > xiter) or (x1 < x2 and x2 < xiter) or (y1 > y2 and y2 > yiter) or (y1 < y2 and y2 < yiter)) # Basically checking to see if we've over-stepped the endpoints of the line
+			else :
+				x1 = points[a][0]
+				y1 = points[a][1]
+				r1 = points[a][2]
+				g1 = points[a][3]
+				b1 = points[a][4]
+				x2 = points[b][0]
+				y2 = points[b][1]
+				r2 = points[b][2]
+				g2 = points[b][3]
+				b2 = points[b][4]
+				length = math.sqrt(pow((x1-x2), 2) + pow((y1-y2), 2))
+				dx = abs(x1 - x2)/length
+				dy = abs(y1 - y2)/length
+				dr = abs(r1 - r2)/length
+				dg = abs(g1 - g2)/length
+				db = abs(b1 - b2)/length
+				if x1 > x2 :
+					dx *= -1
+				if y1 > y2 :
+					dy *= -1
+				if r1 > r2 :
+					dr *= -1
+				if g1 > g2 :
+					dg *= -1
+				if b1 > b2 :
+					db *= -1
+				xiter = x1
+				yiter = y1
+				riter = r1
+				giter = g1
+				biter = b1
+				flag = True
+				while flag :
+					edges.append([math.ceil(xiter), math.ceil(yiter), riter, giter, biter, 255])
+					xiter += dx
+					yiter += dy
+					riter += dr
+					giter += dg
+					biter += db
+					flag = not((x1 > x2 and x2 > xiter) or (x1 < x2 and x2 < xiter) or (y1 > y2 and y2 > yiter) or (y1 < y2 and y2 < yiter)) # Basically checking to see if we've over-stepped the endpoints of the line
 	for n in edges :
 		for m in edges :
-			if n[0] == m[0] and n[1] == m[1] : # skipping same verticies
-				continue
-			elif n[1] == m[1] :
+			if n[0] != m[0] and n[1] == m[1] : # skipping same verticies
 				length = n[0] - m[0]
-				xiter = n[0]
-				riter = n[2]
-				giter = n[3]
-				biter = n[4]
 				x1 = n[0]
 				x2 = m[0]
 				r1 = n[2]
@@ -300,6 +315,10 @@ def drawtri( points ): # This is gong to be a little different from linec, since
 					dg *= -1
 				if b1 > b2 :
 					db *= -1
+				xiter = x1
+				riter = r1
+				giter = g1
+				biter = b1
 				if xiter > m[0] :
 					while xiter > m[0] :
 						xiter += dx
@@ -310,11 +329,34 @@ def drawtri( points ): # This is gong to be a little different from linec, since
 				if xiter < m[0] :
 					while xiter < m[0] :
 						xiter -= dx
-						riter += dr
-						giter += dg
-						biter += db
+						riter -= dr
+						giter -= dg
+						biter -= db
 						putpixel((math.ceil(xiter), math.ceil(n[1])), (riter, giter, biter, 255))
 
+def fann( parse ) :
+	# Get our vertecies
+	base = parse[2]
+	for n in range(3, len(parse)-1) :
+		parse2 = []
+		parse2.append("fann")
+		parse2.append(base)
+		parse2.append(parse[n])
+		parse2.append(parse[n+1])
+		print parse2
+		trig( parse2 )
+
+def stripn( parse ) :
+	# Get our vertecies
+	print len(vertexList)
+	for n in range(2, len(parse)-2) :
+		parse2 = []
+		parse2.append("stripn")
+		parse2.append(parse[n])
+		parse2.append(parse[n+1])
+		parse2.append(parse[n+2])
+		print parse2
+		trig( parse2 )
 
 
 
@@ -357,5 +399,11 @@ while (line != "") :
 		cubicc(parse)
 	elif parse[0] == "beznc" :
 		beznc(parse)
+	elif parse[0] == "cubicg" :
+		cubicg(parse)
+	elif parse[0] == "fann" :
+		fann(parse)
+	elif parse[0] == "stripn" :
+		stripn(parse)
 	line = fread.readline()
 img.save(fileName)
